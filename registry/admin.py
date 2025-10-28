dd58811e917b187425307acc86fbebf4bfccc8c8
Fabio Tranchitella <fabio@tranchitella.eu>
2025-10-28 17:39:32 +0300

    feat: add audit logs
    Signed-off-by: Fabio Tranchitella <fabio@tranchitella.eu>

--- registry/admin.py
+++ registry/admin.py
@@ -1,13 +1,15 @@
+from auditlog.mixins import AuditlogHistoryAdminMixin
 from django.contrib import admin
 
 from .models import Device
 
 
 @admin.register(Device)
-class DeviceAdmin(admin.ModelAdmin):
+class DeviceAdmin(AuditlogHistoryAdminMixin, admin.ModelAdmin):
     list_display = (
         "id",
         "name",
     )
     search_fields = ("id", "name")
     list_filter = ("id", "name")
+    show_auditlog_history_link = True
--- registry/models.py
+++ registry/models.py
@@ -1,8 +1,10 @@
 import uuid
 
+from auditlog.registry import auditlog
 from django.db import models
 
 
+@auditlog.register()
 class Device(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
